2738db8054fdb59a20ff6c6e80e9d5d4c5486115
NSStoyanova22 <NSStoyanova22@codingburgas.bg>
2024-12-17 00:16:53 +0200

    Finish dashboard ui

--- Lyrica/src/dashboard.cpp
+++ Lyrica/src/dashboard.cpp
@@ -6,9 +6,22 @@ dashboard::dashboard(QWidget *parent)
     , ui(new Ui::dashboard)
 {
     ui->setupUi(this);
-
+    QPixmap card(":/images/assets/card.png");
     QPixmap backgroundPix(":/images/assets/dashboardBg.png");
     ui->background->setPixmap(backgroundPix.scaled(1500, 800, Qt::KeepAspectRatio));
+    QList<QLabel*> addCardList = { ui->set1, ui->set2 ,ui->set3, ui->set4 };
+    QList<QLabel*> createCardList = { ui->customSet1, ui->customSet2 ,ui->customSet3, ui->customSet4 };
+
+    for (QLabel* label : addCardList) {
+        if (label) {
+            label->setPixmap(card.scaled(300, 157, Qt::KeepAspectRatio));
+        }
+    }
+    for (QLabel* label : createCardList) {
+        if (label) {
+            label->setPixmap(card.scaled(300, 157, Qt::KeepAspectRatio));
+        }
+    }
 }
 
 dashboard::~dashboard()
--- Lyrica/src/main.cpp
+++ Lyrica/src/main.cpp
@@ -1,11 +1,11 @@
 #include "lib/mainwindow.h"
-#include "lib/dashboard.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    dashboard w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
