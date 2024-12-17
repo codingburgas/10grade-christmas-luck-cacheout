1972346cda936b07269b3ebaa91f72ae6d7201b7
niribareva22 <niribareva22@codingburgas.bg>
2024-12-17 20:53:23 +0200

    Add stylesheets to ui elements.

--- Lyrica/src/main.cpp
+++ Lyrica/src/main.cpp
@@ -1,13 +1,11 @@
 #include "lib/mainwindow.h"
-#include "lib/signinform.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    signinForm w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
